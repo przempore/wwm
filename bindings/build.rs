50adf878c7a548df37b49a9401bab52aef5726e9
Przemek <przempore@gmail.com>
2021-02-07 00:01:16 +0100

    move mouse to right bottom corner of foreground window

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     windows::build!(
-        windows::win32::windows_and_messaging::{GetWindowTextW, GetForegroundWindow}
-    );
+        windows::win32::display_devices::RECT,
+        windows::win32::windows_and_messaging::{GetWindowTextW, GetForegroundWindow, SetWindowPos, GetWindowRect}
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,60 @@
-use bindings::windows::win32::windows_and_messaging::{GetForegroundWindow, GetWindowTextW};
+use crate::tray_icon::show_tray_icon;
+use bindings::windows::{
+    win32::{
+        display_devices::RECT,
+        windows_and_messaging::{GetForegroundWindow, GetWindowRect, GetWindowTextW, HWND},
+    },
+    TRUE,
+};
+use mouse_rs::Mouse;
 
-fn main() {
-    match get_foreground_window_title() {
-        Err(_e) => println!(""),
+mod tray_icon;
+
+fn main() -> Result<(), systray::Error> {
+    let window: HWND;
+    unsafe {
+        window = GetForegroundWindow();
+    }
+
+    match get_window_title(window) {
+        Err(e) => println!("{:?}", e),
         Ok(title) => println!("Title: {}", title),
     };
-}
 
-fn get_foreground_window_title() -> Result<String, ()> {
+    let mut rect = RECT {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+    };
+
     unsafe {
-        let mut text: [u16; 512] = [0; 512];
-        let window = GetForegroundWindow();
-        let len = GetWindowTextW(window, text.as_mut_ptr(), text.len() as i32);
-        let text = String::from_utf16_lossy(&text[..len as usize]);
-
-        if !text.is_empty() {
-            return Ok(text);
+        let success = GetWindowRect(window, &mut rect);
+        if success != TRUE {
+            println!("GetWindowRect failed!",);
+            return Ok(());
         }
     }
 
+    println!("{:?}", rect);
+
+    let mouse = Mouse::new();
+    mouse
+        .move_to(rect.right - 6, rect.bottom - 6)
+        .expect("Unable to move mouse");
+
+    show_tray_icon()
+}
+
+fn get_window_title(window: HWND) -> Result<String, ()> {
+    unsafe {
+        let mut text: [u16; 512] = [0; 512];
+        let len = GetWindowTextW(window, text.as_mut_ptr(), text.len() as i32);
+        let title = String::from_utf16_lossy(&text[..len as usize]);
+
+        if !text.is_empty() {
+            return Ok(title);
+        }
+    }
     Err(())
 }
