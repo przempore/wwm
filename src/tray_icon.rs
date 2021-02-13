e74e0e1d46deeea5ccdae54ee6847ad1a4025b6a
Przemek <przempore@gmail.com>
2021-02-13 15:39:13 +0100

    tray icon

---
+++ src/tray_icon.rs
@@ -0,0 +1,42 @@
+pub fn show_tray_icon() -> Result<(), systray::Error> {
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create window!"),
+    }
+    let err = app.set_icon_from_file(
+        &"C:\\Users\\przem\\Projects\\rust\\systray-rs\\resources\\rust.ico".to_string(),
+    );
+    if err.is_err() {
+        return err;
+    }
+    let err = app.set_tooltip(&"Whatever".to_string());
+    if err.is_err() {
+        return err;
+    }
+
+    app.add_menu_item("Print a thing", |_| {
+        println!("Printing a thing!");
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_item("Add Menu Item", |window| {
+        window.add_menu_item("Interior item", |_| {
+            println!("what");
+            Ok::<_, systray::Error>(())
+        })?;
+        window.add_menu_separator()?;
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_separator()?;
+
+    app.add_menu_item("Quit", |window| {
+        window.quit();
+        Ok::<_, systray::Error>(())
+    })?;
+
+    println!("Waiting on message!");
+    app.wait_for_message()?;
+    Ok(())
+}
